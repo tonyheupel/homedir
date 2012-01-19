9a57ed3e74b7d91a40da55388ed19d07a37ee51d
Tony Heupel <tonyheupel@gmail.com>
2012-01-18 16:14:56 -0800

    assume cloned to a folder that is not the actual homedir

---
+++ link_homedir.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+function set_script_dir {
+  # base folder of this script: http://stackoverflow.com/questions/192319/in-the-bash-script-how-do-i-know-the-script-file-name 
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  echo $DIR
+}
+DIR=$(set_script_dir)
+cd ~
+ln -s $DIR/.bash_profile 
+ln -s $DIR/.vim
+ln -s $DIR/.vimrc 
+
