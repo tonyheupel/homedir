44e454474c2465bb113ee2b511fe894f4ef06571
Tony Heupel <tonyheupel@gmail.com>
2012-07-05 09:29:12 -0700

    remember to update submodules too!

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 cd ~
 git clone http://github.com/tchype/homedir .homedir
 cd .homedir
+git submodule update --init
 ./setup.ps1
 cd ~
 
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Install the latest version of homedir
 cd ~
 git clone git@github.com:tchype/homedir.git .homedir
+cd .homedir
+git submodule update --init
+cd ~
 
 # Link the files and folders in .homedir as if they were in
 # your ~ folder
