21fecea2670b491b8b2f7bb99491e5e5b75c9eef
Tony Heupel <tonyheupel@gmail.com>
2012-07-04 22:46:05 -0700

    make script to setup windows properly

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,10 @@
 Remove-Item ~/_vimrc
-Remove-Item ~/vimfiles -Recurse -Force
+Remove-Item ~/vimfiles -recurse -force
 
 Copy-Item .vimrc ~/_vimrc
-Copy-Item .vim ~/vimfiles -Recurse -Force
+Copy-Item .vim ~/vimfiles -recurse -force
 
+New-Item ~/vimfiles/_backup -type directory -force
+New-Item ~/vimfiles/_temp -type directory -force
+
+(Get-Content ~/_vimrc) -replace "~/.vim/","~/vimfiles/" | Set-Content ~/_vimrc
