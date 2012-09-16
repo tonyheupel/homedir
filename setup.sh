f2a4cb3e57a41364e99f2378437a570d3d7c93f0
Tony Heupel <tonyheupel@gmail.com>
2012-09-16 15:00:45 -0700

    add back some emacs stuff for now

--- setup.sh
+++ setup.sh
@@ -22,6 +22,8 @@ mv -f ~/.bash_profile $DIR/backups/
 mv -f ~/.tm_properties $DIR/backups/
 mv -f ~/.vim $DIR/backups/
 mv -f ~/.vimrc $DIR/backups/
+mv -f ~/.emacs.d $DIR/backups/
+mv -f ~/.emacs $DIR/backups
 mv -f ~/.gitconfig $DIR/backups/
 mv -f ~/.tmux.conf $DIR/backups/
 
@@ -31,6 +33,8 @@ ln -s $DIR/.bash_profile
 ln -s $DIR/.tm_properties
 ln -s $DIR/.vim
 ln -s $DIR/.vimrc
+ln -s $DIR/.emacs.d
+ln -s $DIR/.emacs
 ln -s $DIR/.gitconfig
 ln -s $DIR/.tmux.conf
 
