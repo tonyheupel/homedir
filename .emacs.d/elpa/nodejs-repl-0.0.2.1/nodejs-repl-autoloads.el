942f77d8e292ca34844b5787b7c7bd51114384f7
Tony Heupel <tony@heupel.net>
2013-05-28 18:09:57 -0700

    add emacs modes and update TextMate 2 for RVM

---
+++ .emacs.d/elpa/nodejs-repl-0.0.2.1/nodejs-repl-autoloads.el
@@ -0,0 +1,28 @@
+;;; nodejs-repl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (nodejs-repl) "nodejs-repl" "nodejs-repl.el" (20899
+;;;;;;  35104))
+;;; Generated autoloads from nodejs-repl.el
+
+(autoload 'nodejs-repl "nodejs-repl" "\
+Run Node.js REPL.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("nodejs-repl-pkg.el") (20899 35104 749188))
+
+;;;***
+
+(provide 'nodejs-repl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; nodejs-repl-autoloads.el ends here
